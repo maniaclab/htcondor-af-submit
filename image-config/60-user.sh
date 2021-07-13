d3eef20a641d2931ed49932ca3f80bf9d31552d1
Lincoln Bryant <lincolnb@hep.uchicago.edu>
2021-07-13 16:35:12 -0500

    make direcotry

--- image-config/60-user.sh
+++ image-config/60-user.sh
@@ -5,5 +5,6 @@ set -eo pipefail
 ###############################################################################
 # Grab the connect provisioner 
 CONNECT_DIR='/usr/local/etc/ciconnect'
+mkdir -p $CONNECT_DIR
 curl -L https://raw.githubusercontent.com/maniaclab/ci-connect-api/master/resources/provisioner/sync_users.sh > $CONNECT_DIR/sync_users.sh
 chmod +x $CONNECT_DIR/sync_users.sh
