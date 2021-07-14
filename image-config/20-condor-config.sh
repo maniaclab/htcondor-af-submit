3a0468d711ee9eb53497a8f4e1e6a820453c08c6
Lincoln Bryant <lincolnb@hep.uchicago.edu>
2021-07-14 13:22:08 -0500

    add condor host

--- image-config/20-condor-config.sh
+++ image-config/20-condor-config.sh
@@ -2,9 +2,4 @@
 set -eo pipefail
 
 echo "# This file was created by $prog" > /etc/condor/config.d/01-env.conf
-add_values_to 01-env.conf \
-    CONDOR_HOST "${CONDOR_SERVICE_HOST:-${CONDOR_HOST:-\$(FULL_HOSTNAME)}}" \
-    NUM_CPUS "${NUM_CPUS:-1}" \
-    MEMORY "${MEMORY:-1024}" \
-    RESERVED_DISK "${RESERVED_DISK:-1024}" \
-    USE_POOL_PASSWORD "${USE_POOL_PASSWORD:-no}"
+echo "${CONDOR_SERVICE_HOST:-${CONDOR_HOST:-\$(FULL_HOSTNAME)}}" >> /etc/condor/config.d/01-env.conf
