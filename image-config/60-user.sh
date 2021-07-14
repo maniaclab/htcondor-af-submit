e81992f48a4e79b8d1704e73482d6eb492a4da1c
Lincoln Bryant <lincolnb@hep.uchicago.edu>
2021-07-14 13:35:48 -0500

    updated user config

--- image-config/60-user.sh
+++ image-config/60-user.sh
@@ -4,7 +4,10 @@ set -eo pipefail
 
 ###############################################################################
 # Grab the connect provisioner 
-CONNECT_DIR='/usr/local/etc/ciconnect'
+CONNECT_DIR='/usr/local/sbin'
 mkdir -p $CONNECT_DIR
 curl -L https://raw.githubusercontent.com/maniaclab/ci-connect-api/master/resources/provisioner/sync_users.sh > $CONNECT_DIR/sync_users.sh
 chmod +x $CONNECT_DIR/sync_users.sh
+
+# do a one-time run at startup
+$CONNECT_DIR/sync_users.sh -u root.atlas-af -g root.atlas-af -e https://api.ci-connect.net:18080 >> /var/log/provisioner.log 2>&1
