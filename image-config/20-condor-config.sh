6523438e2b9febef7e59d54fe16cffd35155ac0a
Lincoln Bryant <lincolnb@hep.uchicago.edu>
2021-07-14 13:22:27 -0500

    simplify?

--- image-config/20-condor-config.sh
+++ image-config/20-condor-config.sh
@@ -2,4 +2,4 @@
 set -eo pipefail
 
 echo "# This file was created by $prog" > /etc/condor/config.d/01-env.conf
-echo "${CONDOR_SERVICE_HOST:-${CONDOR_HOST:-\$(FULL_HOSTNAME)}}" >> /etc/condor/config.d/01-env.conf
+echo "${CONDOR_HOST:-\$(FULL_HOSTNAME)}" >> /etc/condor/config.d/01-env.conf
