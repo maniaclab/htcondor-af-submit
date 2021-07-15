ef83ffab50a6aeec123a8af68bac45a36fa5fea6
Lincoln Bryant <lincolnb@hep.uchicago.edu>
2021-07-15 15:51:17 -0500

    try changing condor's gid to match the htcondor container from upstream

--- image-config/20-condor-config.sh
+++ image-config/20-condor-config.sh
@@ -4,5 +4,15 @@ set -eo pipefail
 echo "# This file was created by $prog" > /etc/condor/config.d/01-env.conf
 echo "CONDOR_HOST=${CONDOR_HOST:-\$(FULL_HOSTNAME)}" >> /etc/condor/config.d/01-env.conf
 
+# Change Condor's GID/UID 
+groupmod -g 64 condor
+usermod -u 64 condor
+
+# Fix various perms
+chown condor:condor /var/lib/condor
+chown condor:condor /var/lib/condor/execute
+chown condor:condor /var/lib/condor/spool
+chown :condor /var/lib/condor/oauth_credentials
+
 # Chown the scratch dir
 chown -R condor: /scratch
