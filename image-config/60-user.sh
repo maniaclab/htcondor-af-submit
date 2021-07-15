b2d25c0a2e509da7be4c1b9059cd92edee2ae896
Lincoln Bryant <lincolnb@hep.uchicago.edu>
2021-07-15 16:06:06 -0500

    if we dont start from a directory then the provisioner just litters the rootfs with random files

--- image-config/60-user.sh
+++ image-config/60-user.sh
@@ -4,10 +4,12 @@ set -eo pipefail
 
 ###############################################################################
 # Grab the connect provisioner 
-CONNECT_DIR='/usr/local/sbin'
+CONNECT_DIR='/usr/local/ciconnect'
 mkdir -p $CONNECT_DIR
 curl -L https://raw.githubusercontent.com/maniaclab/ci-connect-api/master/resources/provisioner/sync_users.sh > $CONNECT_DIR/sync_users.sh
 chmod +x $CONNECT_DIR/sync_users.sh
 
 # do a one-time run at startup
+pushd $CONNECT_DIR
 $CONNECT_DIR/sync_users.sh -u root.atlas-af -g root.atlas-af -e https://api.ci-connect.net:18080 >> /var/log/provisioner.log 2>&1
+popd
