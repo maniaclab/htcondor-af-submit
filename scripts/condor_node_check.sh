963c6b520d9b0cfc29e251f50c23d0204395810e
Farnaz Golnaraghi <farnazgolnaraghi5@gmail.com>
2022-02-22 21:24:53 +0000

    edited the path for condor node check config

--- scripts/condor_node_check.sh
+++ scripts/condor_node_check.sh
@@ -4,4 +4,4 @@ full_node=$(hostname)
 IFS='.' read -r nodename af uchi edu <<< "${full_node}"
 
 /bin/sysclient online "${nodename}"
-/bin/condor_node_check --config-file /etc/sysview.ini
+/bin/condor_node_check --config-file /etc/sysview/sysview.ini
