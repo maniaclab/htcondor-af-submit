a109f76a2900202304386279a7ea464c9cc06448
Farnaz Golnaraghi <farnazgolnaraghi5@gmail.com>
2022-02-24 01:38:19 +0000

    making a test text file with condor_node_check.sh again

--- scripts/condor_node_check.sh
+++ scripts/condor_node_check.sh
@@ -4,4 +4,5 @@ full_node=$(hostname)
 IFS='.' read -r nodename af uchi edu <<< "${full_node}"
 
 /bin/sysclient online "${nodename}"
+echo 'Hello, world.' > /tmp/foo.txt
 /bin/condor_node_check --config-file /etc/sysview/sysview.ini
