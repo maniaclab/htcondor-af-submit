b991eee80146a0428479816b600be3c292ccb476
Farnaz Golnaraghi <farnazgolnaraghi5@gmail.com>
2022-02-24 17:03:12 +0000

    removed the test text file from condor node check

--- scripts/condor_node_check.sh
+++ scripts/condor_node_check.sh
@@ -4,5 +4,4 @@ full_node=$(hostname)
 IFS='.' read -r nodename af uchi edu <<< "${full_node}"
 
 /bin/sysclient online "${nodename}"
-echo 'Hello, world.' > /tmp/foo.txt
 /bin/condor_node_check --config-file /etc/sysview/sysview.ini
