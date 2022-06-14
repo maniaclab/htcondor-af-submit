a4ce30fbf84332f008c1f3d88c0a812c222eebde
Lincoln Bryant <lincolnb@uchicago.edu>
2022-06-14 07:52:21 -0500

    add token

--- image-config/60-user.sh
+++ image-config/60-user.sh
@@ -11,9 +11,10 @@ chmod +x $CONNECT_DIR/sync_users.sh
 
 # do a one-time run at startup
 pushd $CONNECT_DIR
+echo "token=$API_TOKEN" > $CONNECT_DIR/token
+export API_TOKEN_FILE=$CONNECT_DIR/token
 $CONNECT_DIR/sync_users.sh -u root.atlas-af -g root.atlas-af -e https://api.ci-connect.net:18080
 popd
 
 echo 'export PATH="$PATH:/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin:/usr/local/sbin"' >> $CONNECT_DIR/config
 echo "export API_TOKEN_FILE=$CONNECT_DIR/token" >> $CONNECT_DIR/config
-echo "token=$API_TOKEN" > $CONNECT_DIR/token
